abe5382726e437c074e32f25fde83b65da2971da
x123y123 <a0921444212@gmail.com>
2022-11-20 00:22:41 +0800

    pthread_cond_t practice

--- pthread/pthread_02.c
+++ pthread/pthread_02.c
@@ -1,3 +1,10 @@
+/* pthread_cond_t simple test thread ordering */
+
+
+// int pthread_cond_signal(&mutex)
+// int pthread_cond_wait(&cond, &mutex)
+
+
 #include <stdio.h>
 #include <pthread.h>
 
@@ -5,20 +12,26 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 int done = 0;
 
+void *w1()
+{
+/*  block this thread until another thread signals cond. 
+    While blocked, the mutex is released, 
+    then re-acquired before this thread is woken up and the call returns.  */
+   
+
+    pthread_cond_wait(&cond, &mutex);  
+    printf("word!\n");
+}
+
 void *w2()
 {
     pthread_mutex_lock(&mutex);
     printf("hello ");
     done++;
-    pthread_cond_signal(&cond);
+    pthread_cond_signal(&cond);                 // wait up the main thread (if it is sleeping) to test the value of done
     pthread_mutex_unlock(&mutex);
 }
 
-void *w1()
-{
-    pthread_cond_wait(&cond, &mutex);
-    printf("word!\n");
-}
 
 int main()
 {
